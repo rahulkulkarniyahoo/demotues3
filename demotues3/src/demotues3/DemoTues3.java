dfe6223038f264973a1d3f47ca105e75b42e92be
Rahul Kulkarni <Rahul.Kulkarni@mailnewspapers.co.uk>
2015-10-27 10:58:00 +0000

    Squashed commit of the following:
    commit a5112a4fa553213ced0d6d9e6a3884aabc6b0e74
    Author: Rahul Kulkarni <Rahul.Kulkarni@mailnewspapers.co.uk>
    Date:   Tue Oct 27 10:57:46 2015 +0000
    
    	Commit from Mac Feature 1

--- demotues3/src/demotues3/DemoTues3.java
+++ demotues3/src/demotues3/DemoTues3.java
@@ -5,6 +5,11 @@ public class DemoTues3 {
 	public static void main(String[] args) {
 		 System.out.println("this is test");
 		 System.out.println("this is test - Develop created");
+		 System.out.println("this is test - Mac F1_1");
+		 System.out.println("this is test - Mac F1_2");
+		 System.out.println("this is test - Mac F1_3");
+		 System.out.println("this is test - Mac F1_4");
+		 
 	}
 
 }
